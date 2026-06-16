cf0025d6861d93816d48ed75c5bd30948b245d3d
Alejandro Villar <avillar@ogc.org>
2026-06-16 22:11:03 +0200

    Version v0.4.3

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -456,6 +456,17 @@ async function augment(rootElem, context, options = {}) {
                 return { '@id': `${closestBase}${term}` };
             }
         }
+        else {
+            const idx = term.indexOf(':');
+            const prefix = term.substring(0, idx);
+            const localPart = term.substring(idx + 1);
+            if (!localPart.startsWith('//')) {
+                const resolvedPrefix = resolveTerm(prefix, contextStack);
+                if (resolvedPrefix !== null && '@id' in resolvedPrefix && typeof resolvedPrefix['@id'] === 'string') {
+                    return { '@id': `${resolvedPrefix['@id']}${localPart}` };
+                }
+            }
+        }
         return null;
     };
     const findPropertyChildren = (elem) => {
